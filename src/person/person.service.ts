07928b2f474b29bcf4aeb72d76eedccd8190acd4
eternallycyf <969475322@qq.com>
2023-07-01 19:31:40 +0800

    feat: add restful demo

---
+++ src/person/person.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Injectable()
+export class PersonService {
+  create(createPersonDto: CreatePersonDto) {
+    return createPersonDto;
+  }
+
+  findAll(name: string, age: string) {
+    return { name, age };
+  }
+
+  findOne(id: number) {
+    return `get: 参数 ${id} `;
+  }
+
+  update(id: number, updatePersonDto: UpdatePersonDto) {
+    return `This action updates a #${id} person`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} person`;
+  }
+}
